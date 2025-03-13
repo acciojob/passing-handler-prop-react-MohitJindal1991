b11646ef708bb936fa06ec9c508344c0a8b7316e
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-03-13 11:47:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import "../styles/App.css";
+import "../styles/App.css";
 import Selection from "./Selection";
 import ColourSelector from "./ColourSelector";
 
