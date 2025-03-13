f61fd5f86354b19566bfef9bd04c5dd1119af4bc
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-03-13 11:47:15 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,29 @@
+import React from "react";
+import { useState } from "react";
+
+function Selection({ ele, applyColor }) {
+  const [color, setColor] = useState("");
+  function clickHandler() {
+    applyColor(setColor);
+  }
+  return (
+    <div
+      onClick={clickHandler}
+      style={{
+        width: "200px",
+        height: "200px",
+        border: "1px solid black",
+        backgroundColor: `${color}`,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "19px",
+        fontWeight: "bold",
+      }}
+    >
+      {ele}
+    </div>
+  );
+}
+export default Selection;
 
